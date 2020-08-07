2cc5ba66bf844600b615d50aeb31ee37c9b1de11
Prateek-Singh20 <69199575+Prateek-Singh20@users.noreply.github.com>
2020-08-07 14:34:04 +0530

    Update time.c

--- timer/time.c
+++ timer/time.c
@@ -5,12 +5,12 @@ int delay(void) // function for delay of 100 ms
 	T0MCR=0x00000004; //when MR0 stop
 	T0TCR=1;  //enable timer 0
 	while(T0TC!=T0MR0);
-	T0TC=0; // resetv timer
+	T0TC=0; // reset timer
 	return 0;
 }
 int main(void)
 {
-	IO0DIR |=(1<<11); //intializing pin  P0.10 as output 
+	IO0DIR |=(1<<11); //intializing pin  P0.11 as output 
 	while(1)
 	{
 		IO0SET=(1<<11); //Turn LED on
